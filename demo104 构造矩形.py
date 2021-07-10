12a973d20920134d53d61df99bcfe0178b5a2bb6
nfx_git <13561592791@163.com>
2021-07-10 21:27:33 +0800

    构造矩形

--- demo104 构造矩形.py
+++ demo104 构造矩形.py
@@ -4,10 +4,11 @@ import math
 
 
 def constructRectangle(area):
-    w=int(math.sqrt(area))  #宽度一定小于等于面积的开平方，因为长度必须大于等于宽度
-    while area%w!=0:    #找到第一个可以整除的便是答案
-        w-=1
-    return [area//w,w]
+    w = int(math.sqrt(area))  # 宽度一定小于等于面积的开平方，因为长度必须大于等于宽度
+    while area % w != 0:  # 找到第一个可以整除的便是答案
+        w -= 1
+    return [area // w, w]
 
-area=17
+
+area = 17
 print(constructRectangle(area))
