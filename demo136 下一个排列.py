dcd9268377418222aafac13cd50acac67ce6b466
nfx_git <13561592791@163.com>
2021-08-09 21:28:27 +0800

    下一个排列

---
+++ demo136 下一个排列.py
@@ -0,0 +1,24 @@
+# 作者：宁方笑
+# 开发时间：2021/8/9 21:07
+def nextPermutation(nums):
+    i = len(nums) - 2
+    while i >= 0 and nums[i] >= nums[i + 1]:
+        i -= 1
+
+    if i > 0:
+        j = len(nums) - 1
+        while j >= 0 and nums[i] >= nums[j]:
+            j -= 1
+        nums[i], nums[j] = nums[j], nums[i]
+
+    left, right = i + 1, len(nums) - 1
+    while left < right:
+        nums[left], nums[right] = nums[right], nums[left]
+        left += 1
+        right -= 1
+
+    return nums
+
+
+nums = [1,1,5]
+print(nextPermutation(nums))
