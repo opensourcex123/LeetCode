a0660acf38b49b8ef61b47a85092835089302fd7
nfx_git <13561592791@163.com>
2021-08-25 21:16:09 +0800

    插入区间

---
+++ demo152 插入区间.py
@@ -0,0 +1,34 @@
+# 作者：宁方笑
+# 开发时间：2021/8/25 20:59
+def insert(intervals, newInterval):
+    # intervals.append(newInterval)
+    # intervals.sort(key=lambda x: x[0])
+    #
+    # merged = []
+    # for interval in intervals:
+    #     if not merged or merged[-1][1] < interval[0]:
+    #         merged.append(interval)
+    #     else:
+    #         merged[-1][1] = max(merged[-1][1], interval[1])
+    #
+    # return merged
+    left, right = newInterval
+    res = []
+    placed = False
+    for li, ri in intervals:
+        if li > right:
+            if not placed:
+                res.append([left, right])
+                placed = True
+            res.append([li, ri])
+        elif ri < left:
+            res.append([li, ri])
+        else:
+            left = min(li, left)
+            right = max(ri, right)
+    return res
+
+
+intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]]
+newInterval = [4,8]
+print(insert(intervals, newInterval))
